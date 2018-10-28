ef373a9c0f7eff91b09093687e8c2dd0e60fa146
skitterm <curlyhairiscrucial@gmail.com>
2018-10-27 17:47:53 -0700

    UI added to the Create Goals screen. Save button is enabled/disabled depending on fields being filled out.

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,19 +1,100 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Picker,
+  DatePickerIOS,
+  TextInput,
+  KeyboardAvoidingView
+} from 'react-native';
 
 class CreateScreen extends Component {
   static navigationOptions = {
     title: 'Create a Goal'
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      frequency: 'daily',
+      deadline: new Date()
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Text>Create</Text>
-        <Button title="Create Goal" onPress={this.createGoal} />
-      </View>
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={{
+          flex: 1
+        }}
+      >
+        <TextInput
+          placeholder="Goal title"
+          style={styles.text}
+          value={this.state.title}
+          onChangeText={this.onTitleChange}
+        />
+        <TextInput
+          placeholder="More about your goal..."
+          style={[styles.text, styles.multilineText]}
+          value={this.state.description}
+          onChangeText={this.onDescriptionChange}
+        />
+        <View>
+          <Picker
+            selectedValue={this.state.frequency}
+            onValueChange={this.onFrequencyChange}
+          >
+            <Picker.Item label="Daily" value="daily" />
+            <Picker.Item label="Weekly" value="weekly" />
+            <Picker.Item label="Monthly" value="monthly" />
+            <Picker.Item label="One-time" value="one-time" />
+          </Picker>
+          <DatePickerIOS
+            date={this.state.deadline}
+            minimumDate={new Date()}
+            onDateChange={() => {}}
+            minuteInterval={30}
+            style={{
+              display: this.state.frequency === 'one-time' ? 'flex' : 'none'
+            }}
+          />
+        </View>
+        <Button
+          title="Create Goal"
+          disabled={this.state.title.length === 0}
+          onPress={this.createGoal}
+        />
+      </KeyboardAvoidingView>
     );
   }
+
+  onTitleChange = text => {
+    this.setState({
+      title: text
+    });
+  };
+
+  onDescriptionChange = text => {
+    this.setState({
+      description: text
+    });
+  };
+
+  onFrequencyChange = frequency => {
+    this.setState({
+      frequency
+    });
+  };
+
+  onDeadlineChange = () => {
+    this.onUpdate();
+  };
 
   createGoal = () => {
     if (this.props.screenProps.database) {
@@ -41,3 +122,16 @@ class CreateScreen extends Component {
 }
 
 export default CreateScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 28,
+    backgroundColor: 'white',
+    paddingVertical: 5,
+    paddingHorizontal: 30,
+    marginVertical: 10
+  },
+  multilineText: {
+    fontSize: 18
+  }
+});
