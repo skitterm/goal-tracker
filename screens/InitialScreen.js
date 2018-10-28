88de878c532cef7d5ef642d3dbbfa341efc897c2
skitterm <curlyhairiscrucial@gmail.com>
2018-10-27 21:55:28 -0700

    Initial and list screens created.

--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -9,10 +9,29 @@ class InitialScreen extends Component {
   render() {
     return (
       <View>
-        <Text>Initial</Text>
+        <Text
+          style={{
+            marginTop: 80,
+            fontSize: 36,
+            fontWeight: 'bold',
+            textAlign: 'center'
+          }}
+        >
+          Welcome to Goal Tracker!
+        </Text>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: 'center',
+            marginTop: 30,
+            marginBottom: 60
+          }}
+        >
+          We're glad you're here. Click below to begin.
+        </Text>
         <Button
-          title="To Settings"
-          onPress={() => this.props.navigation.navigate('List')}
+          title="Create My First Goal"
+          onPress={() => this.props.navigation.navigate('Create')}
         />
       </View>
     );
--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View,  Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SegmentedControlIOS,
+  FlatList
+} from 'react-native';
 
 class ListScreen extends Component {
-  static navigationOptions = {
-      title: 'Goals List'
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Goals List',
+      headerRight: (
+        <Button
+          title="Create"
+          onPress={() => {
+            // segueToCreate doesn't exist initially
+            navigation.getParam('segueToCreate')();
+          }}
+        />
+      )
+    };
   };
 
   render() {
-      return (
+    return (
       <View>
-          <Text>List</Text>
-          <Button 
-            title="To Settings"            
-            onPress={() => this.props.navigation.navigate('Details')}  
-          />          
+        <FlatList
+          style={{ backgroundColor: 'white' }}
+          data={[
+            { key: 'Brush Teeth' },
+            { key: 'Eat Breakfast' },
+            { key: 'Exercise' }
+          ]}
+          renderItem={({ item }) => (
+            <Text
+              style={{
+                fontSize: 18,
+                color: '#333',
+                paddingVertical: 15,
+                paddingHorizontal: 15
+              }}
+            >
+              {item.key}
+            </Text>
+          )}
+          ItemSeparatorComponent={SomethingElse}
+          ListHeaderComponent={
+            <SegmentedControlIOS
+              style={{ marginHorizontal: 15, marginVertical: 10 }}
+              values={['D', 'W', 'M', '1x']}
+              selectedIndex={0}
+            />
+          }
+        />
       </View>
-      );
+    );
+  }
+
+  componentDidMount = () => {
+    this.props.navigation.setParams({
+      segueToCreate: this.segueToCreate
+    });
+  };
+
+  segueToCreate = () => {
+    this.props.navigation.navigate('Create');
+  };
+}
+
+class SomethingElse extends Component {
+  render() {
+    return <View style={{ borderTopColor: '#eee', borderTopWidth: 1 }} />;
   }
 }
 
